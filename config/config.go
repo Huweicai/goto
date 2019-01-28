f55f9241e9ba6c5b64bbdd51f73c87dd1c09718f
Richard Hu <huweicai@bytedance.com>
2019-01-28 17:57:20 +0800

    [optimize] structure optimize

--- config/config.go
+++ config/config.go
@@ -19,6 +19,7 @@ const (
 	TypeScalar
 )
 
+//the core config
 type Nest struct {
 	Data map[string]interface{}
 }
@@ -45,7 +46,30 @@ func NewNest(configPath string) (*Nest, error) {
 	return &Nest{Data: conf}, nil
 }
 
-//载入配置
+func (n *Nest) GetScalar(paths []string) (URL string, ok bool) {
+	return getScalar(paths, n.Data)
+}
+
+func (n *Nest) Flush() error {
+	return writeYaml(GetConfigPath(), n.Data)
+}
+
+func (n *Nest) AddScalar(paths []string, url string) {
+	addScalar(n.Data, paths, url)
+}
+
+func typeOf(i interface{}) ModelType {
+	switch reflect.TypeOf(i).Kind() {
+	case reflect.Map:
+		return TypeVector
+	case reflect.String:
+		return TypeScalar
+	default:
+		return TypeUnknown
+	}
+}
+
+//load config from yaml
 func loadYaml(path string) (conf map[string]interface{}, err error) {
 	f, err := ioutil.ReadFile(path)
 	if err != nil {
@@ -62,24 +86,13 @@ func loadYaml(path string) (conf map[string]interface{}, err error) {
 	return
 }
 
+//put the config into yaml file
 func writeYaml(path string, target interface{}) error {
 	out, err := yaml.Marshal(target)
 	if err != nil {
 		return err
 	}
 	return ioutil.WriteFile(path, out, 0644)
-}
-
-func (n *Nest) GetScalar(paths []string) (URL string, ok bool) {
-	return getScalar(paths, n.Data)
-}
-
-func (n *Nest) Flush() error {
-	return writeYaml(GetConfigPath(), n.Data)
-}
-
-func (n *Nest) AddScalar(paths []string, url string) {
-	addScalar(n.Data, paths, url)
 }
 
 func addScalar(m map[string]interface{}, paths []string, url string) map[string]interface{} {
@@ -116,34 +129,6 @@ func addScalar(m map[string]interface{}, paths []string, url string) map[string]
 	return m
 }
 
-func toMapStringInterface(m map[interface{}]interface{}) map[string]interface{} {
-	nm := make(map[string]interface{})
-	for k, v := range m {
-		if reflect.TypeOf(v).Kind() == reflect.Map {
-			v = toMapStringInterface(v.(map[interface{}]interface{}))
-		}
-		nm[k.(string)] = v
-	}
-	return nm
-}
-
-func toLower(m map[string]interface{}) map[string]interface{} {
-	for k, v := range m {
-		delete(m, k)
-		switch typeOf(v) {
-		case TypeScalar:
-			v = strings.ToLower(v.(string))
-		case TypeVector:
-			v = toLower(v.(map[string]interface{}))
-		default:
-			continue
-		}
-		k = strings.ToLower(k)
-		m[k] = v
-	}
-	return m
-}
-
 //get the specified URL
 func getScalar(paths []string, m map[string]interface{}) (scalar string, ok bool) {
 	if len(paths) == 0 {
@@ -171,13 +156,32 @@ func getScalar(paths []string, m map[string]interface{}) (scalar string, ok bool
 	}
 }
 
-func typeOf(i interface{}) ModelType {
-	switch reflect.TypeOf(i).Kind() {
-	case reflect.Map:
-		return TypeVector
-	case reflect.String:
-		return TypeScalar
-	default:
-		return TypeUnknown
+//convert map[interface{}]intreface{} to map[string]interface{}
+func toMapStringInterface(m map[interface{}]interface{}) map[string]interface{} {
+	nm := make(map[string]interface{})
+	for k, v := range m {
+		if reflect.TypeOf(v).Kind() == reflect.Map {
+			v = toMapStringInterface(v.(map[interface{}]interface{}))
+		}
+		nm[k.(string)] = v
 	}
+	return nm
+}
+
+//convert all the upper case to lower in both k and v
+func toLower(m map[string]interface{}) map[string]interface{} {
+	for k, v := range m {
+		delete(m, k)
+		switch typeOf(v) {
+		case TypeScalar:
+			v = strings.ToLower(v.(string))
+		case TypeVector:
+			v = toLower(v.(map[string]interface{}))
+		default:
+			continue
+		}
+		k = strings.ToLower(k)
+		m[k] = v
+	}
+	return m
 }
