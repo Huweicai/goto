41ffbe9af14b95e4e86155ee286bde8b1f04b4cc
Richard Hu <huweicai@bytedance.com>
2019-02-19 13:52:36 +0800

    [fix] not copy URL 2

--- handler/get.go
+++ handler/get.go
@@ -4,7 +4,6 @@ import (
 	"fmt"
 	"github.com/Huweicai/goto/config"
 	"log"
-	"net/url"
 	"os/exec"
 )
 
@@ -19,19 +18,13 @@ func Get(args []string) {
 		log.Println("%+v not found", args)
 		return
 	}
-	_, uErr := url.Parse(value)
-	if uErr != nil {
+	cmd := exec.Command("open", value)
+	//try to open it
+	if err = cmd.Run(); err != nil {
 		//not url
 		//print for copy to clipboard
 		fmt.Print(value)
 		return
-	}
-	cmd := exec.Command("open", value)
-	//print for copy to clipboard
-	fmt.Print(value)
-	//try to open it
-	err = cmd.Run()
-	if err != nil {
 		log.Println(err.Error())
 	}
 }
